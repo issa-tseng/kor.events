46c8ee8338993d8ddf806491e0e420c5dbdb0c9a
Clint Tseng <clint@dontexplain.com>
2011-09-15 01:25:51 -0700

    a test!

---
+++ spec/core_spec.js
@@ -0,0 +1,26 @@
+var ke = require('../kor.events');
+
+describe('basic listening and firing', function()
+{
+    it('should listen and fire a simple verb successfully', function()
+    {
+        var flag = false;
+
+        ke.listen({
+            verb: 'test-event',
+            callback: function()
+            {
+                flag = true;
+            }
+        });
+
+        expect(flag).toBeFalsy();
+
+        ke.fire({
+            verb: 'test-event' 
+        });
+
+        expect(flag).toBeTruthy();
+    });
+});
+
